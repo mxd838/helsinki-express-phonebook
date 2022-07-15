bd80c92fce086d87ef8750e46d9b993c76dbb7ac
mxd838 <mxduran@protonmail.com>
2022-07-15 21:25:40 +0200

    :sparkles: feat: add first GET and DELETE routes

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log('ok')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+let userPhones = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h2>Hola el Mundo</h2>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(userPhones)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <div>
+            <p>Phonebook has info for ${userPhones.length} people</p>
+            <p>${new Date()}</p>
+        </div>
+    `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const userPhoneId = Number(request.params.id)
+    const userPhone = userPhones.find(userphone => userphone.id === userPhoneId)
+    if (userPhone){
+        response.json(userPhone)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const userPhoneId = Number(request.params.id)
+    userPhones = userPhones.filter(userphone => userphone.id !== userPhoneId)
+    response.json(userPhones)
+    response.status(204).end()
+})
+
+// app.post
+
+const PORT = 3001
+app.listen(PORT)
